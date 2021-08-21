7dd7ccae3a7cc3b62f6d779b4803ffeea1cceee6
Dmitri Gordeev <dimapushkin@gmail.com>
2021-08-22 00:15:33 +0300

    makes better fuckin constraints

--- ViewColorApp/ViewController.swift
+++ ViewColorApp/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var viewColor: UIView!
+    
+    @IBOutlet weak var redLabelAmount: UILabel!
+    @IBOutlet weak var greenLAbelAmount: UILabel!
+    @IBOutlet weak var blueLabelAmount: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        viewColor.layer.cornerRadius = 25
+        
+        
+        redLabelAmount.text = String(format: "%.2f", redSlider.value)
+        
+        
     }
 
-
+    @IBAction func redSliderAction() {
+        redLabelAmount.text = String(format: "%.2f", redSlider.value)
+    }
+    
 }
 
