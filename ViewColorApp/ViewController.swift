b0613d90c4c037f37de3be78285531636c22be77
Dmitri Gordeev <dimapushkin@gmail.com>
2021-08-21 16:43:03 +0300

    Initial Commit

---
+++ ViewColorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewColorApp
+//
+//  Created by Повелитель on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
